a6351fbc8474973da23d19b71026307bf40079ef
Anderson Chen <ander.chen00@gmail.com>
2019-03-10 17:02:53 -0300

    Added redux to show list of cities in the db

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions/index';
+
 import City from './city';
 
 
 class CityList extends Component {
+
+  componentWillMount() {
+    this.props.setCities();
+  }
+
   renderList() {
     return this.props.cities.map((city, key) => {
       return (
@@ -21,4 +30,17 @@ class CityList extends Component {
   }
 }
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities },
+    dispatch
+  );
+}
+
+function mapReduxStateToProps(reduxState) {
+  return {
+    cities: reduxState.cities
+  };
+}
+
+export default connect(mapReduxStateToProps, mapDispatchToProps)(CityList);
